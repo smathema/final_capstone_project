380549d11d9e90b74f9552987490ea2881824204
Dipendra Paneru <dipenpaneru@ymail.com>
2019-06-15 14:53:55 -0400

    third commit

--- disaster_recovery/urls.py
+++ disaster_recovery/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
     path('jobcode/', include('JobCode.urls')),
     path('machinemgt/', include('MachineMgt.urls')),
+    path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='JobCode/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='JobCode/logout.html'), name='logout'),
 
--- disaster_recovery/views.py
+++ disaster_recovery/views.py
@@ -1,5 +1,19 @@
-
+from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
+from .forms import UserRegisterForm
 
 class HomePage(TemplateView):
     template_name = "JobCode/index.html"
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'JobCode/register.html', {'form': form})
