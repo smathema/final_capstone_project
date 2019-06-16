440b4e5cddc79b124c082572f0d3aa7e3b3336cf
smathema <smathema@my.bridgeport.edu>
2019-06-16 18:36:02 -0400

    5th co-meet

--- JobCode/views.py
+++ JobCode/views.py
@@ -1,24 +1,25 @@
-from django.shortcuts import render
 from . models import Job
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 class JobListView(ListView):
-    model=Job
+    model = Job
+
 
 class JobDetailView(DetailView):
+    model = Job
 
-    model=Job
 
 class JobCreateView(CreateView):
-    model=Job
-    fields = ['job_code', 'description','hourly_rate','max_hour_per_day']
+    model = Job
+    fields = ['job_code', 'description', 'hourly_rate', 'max_hour_per_day']
+
 
 class JobUpdateView(UpdateView):
-    model=Job
+    model = Job
+    fields = ['job_code', 'description', 'hourly_rate', 'max_hour_per_day']
 
-    fields = ['job_code', 'description','hourly_rate','max_hour_per_day']
 
 class JobDeleteView(DeleteView):
-    model=Job
-    success_url='/'
+    model = Job
+    success_url = '/'
