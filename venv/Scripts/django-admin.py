b9199b1e99ffa551239e60c249716b2596062af0
smathema <smathema@my.bridgeport.edu>
2019-06-24 22:00:53 -0400

    virtual environment removed

--- venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!D:\My Practices\PyCharm Projects\Django projects\git_projects\final_capstone_project\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
